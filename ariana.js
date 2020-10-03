9642360734455e3b50b75b3a91d753f54e474b0f
Aether <64367876+aethersthetix@users.noreply.github.com>
2020-10-03 14:21:59 +0200

    Create ariana.js

---
+++ ariana.js
@@ -0,0 +1,4 @@
+const vengeance = "I will strike down upon thee with great vengeance and furious anger those who attempt to poison and destroy my brothers and sisters. And you will know my name is lord  when I lay my vengeance upon you."
+while (true){
+console.log(vengeance)
+}
